cf8817f3f7abc7b0bc74cef60e82e0a42bfeabc1
SarahWei0804 <a0976395798@gmail.com>
2024-06-26 09:13:09 +0000

    Update with new result

--- train.py
+++ train.py
@@ -10,29 +10,35 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 import skops.io as sio
 
-df = pd.read_csv('data/heart.csv')
+df = pd.read_csv("data/heart.csv")
 X = df.drop("HeartDisease", axis=1)
-y = df['HeartDisease'].values
-train_x, test_x, train_y, test_y = train_test_split(X, y, stratify=y, random_state=0, test_size=0.3)
+y = df["HeartDisease"].values
+train_x, test_x, train_y, test_y = train_test_split(
+    X, y, stratify=y, random_state=0, test_size=0.3
+)
 
-cat_cols = [list(X.columns).index(i) for i in list(X.select_dtypes('object').columns)]
+cat_cols = [list(X.columns).index(i) for i in list(X.select_dtypes("object").columns)]
 num_cols = list(set([i for i in range(len(X.columns))]) - set(cat_cols))
 
-transformer = ColumnTransformer([
-    ('encoder', OrdinalEncoder(), cat_cols),
-    ('num_imputer', SimpleImputer(strategy='median'), num_cols),
-    ('num_scaler', StandardScaler(), num_cols)
-])
-pipe = Pipeline(steps=[
-    ('preprocessing', transformer),
-    ('model', RandomForestClassifier(n_estimators=300, random_state=0))
-])
+transformer = ColumnTransformer(
+    [
+        ("encoder", OrdinalEncoder(), cat_cols),
+        ("num_imputer", SimpleImputer(strategy="median"), num_cols),
+        ("num_scaler", StandardScaler(), num_cols),
+    ]
+)
+pipe = Pipeline(
+    steps=[
+        ("preprocessing", transformer),
+        ("model", RandomForestClassifier(n_estimators=300, random_state=0)),
+    ]
+)
 pipe.fit(train_x.values, train_y)
 
 
 predictions = pipe.predict(test_x.values)
 accuracy = accuracy_score(test_y, predictions)
-f1 = f1_score(test_y, predictions, average='macro')
+f1 = f1_score(test_y, predictions, average="macro")
 
 with open("result/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}.")
